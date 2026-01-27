80c7d1d2d538fba78a2317ef4bd7236366e18ff5
Wasim <wasim@example.com>
2026-01-27 19:45:17 +0530

    hdf

--- pages/api/auth/register.js
+++ pages/api/auth/register.js
@@ -1,4 +1,4 @@
-import dbConnect from "../../../lib/dbConnect";
+import dbConnect from "../../../lib/db";
 import User from "../../../models/User";
 import { hashPassword } from "../../../lib/authUtils";
 
